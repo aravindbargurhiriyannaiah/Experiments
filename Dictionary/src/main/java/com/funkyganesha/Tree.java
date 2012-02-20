5af414d38c81850c7153b541133b4d6455cd379c
Aravind Bargur Hiriyannaiah <aravind@aravind.(none)>
2012-02-19 23:41:37 -0800

    Removed unnecessary comments

--- Dictionary/src/main/java/com/funkyganesha/Tree.java
+++ Dictionary/src/main/java/com/funkyganesha/Tree.java
@@ -7,26 +7,9 @@ import com.funkyganesha.bean.Node;
  *
  */
 public interface Tree {
-    /**
-     * Search within the tree.
-     *
-     * @param data
-     * @return true if data is available in the tree, false otherwise.
-     */
+
     boolean search(int data);
 
-
-    /**
-     * insert into the tree.
-     *
-     *
-     *
-     *
-     *
-     *
-     * @param data
-     * @return
-     */
     void insert(int data);
 
     void remove(Node node);
